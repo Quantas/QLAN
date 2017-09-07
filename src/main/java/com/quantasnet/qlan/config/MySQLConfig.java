e8ee49f9f7bfef49ff737027870d19208e6f628a
Andrew Landsverk <dewdew@gmail.com>
2017-09-07 18:39:58 -0500

    DATABASE_URL parsing

--- src/main/java/com/quantasnet/qlan/config/MySQLConfig.java
+++ src/main/java/com/quantasnet/qlan/config/MySQLConfig.java
@@ -2,12 +2,12 @@ package com.quantasnet.qlan.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.hibernate.dialect.MySQLDialect;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
+import java.net.URI;
 import java.util.Properties;
 
 /**
@@ -17,19 +17,25 @@ import java.util.Properties;
 @Profile("!filedb")
 public class MySQLConfig {
 
-    @Value("#{systemProperties['DATABASE_URL']}")
-    private String databaseUrl;
-
     @Bean(destroyMethod = "close")
     public DataSource dataSource() throws Exception {
         final ComboPooledDataSource jpaDataSource = new ComboPooledDataSource();
+
+        final URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        final String username = dbUri.getUserInfo().split(":")[0];
+        final String password = dbUri.getUserInfo().split(":")[1];
+        final String dbUrl = "jdbc:mysql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
         jpaDataSource.setDriverClass(com.mysql.jdbc.Driver.class.getCanonicalName());
-        jpaDataSource.setJdbcUrl("jdbc:" + databaseUrl);
+        jpaDataSource.setJdbcUrl(dbUrl);
+        jpaDataSource.setUser(username);
+        jpaDataSource.setPassword(password);
         jpaDataSource.setAcquireIncrement(5);
         jpaDataSource.setIdleConnectionTestPeriod(60);
         jpaDataSource.setMaxPoolSize(50);
         jpaDataSource.setMinPoolSize(10);
         jpaDataSource.setMaxStatements(50);
+        
         return jpaDataSource;
     }
 
